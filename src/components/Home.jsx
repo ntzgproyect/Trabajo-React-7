8c173732575cb6c2edfcaedbc691c02076c294d5
Agustin <ntzgproyect@gmail.com>
2023-10-29 19:57:09 -0300

    prueba de perfil

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="container mt-5">
+            <h1>Bienvenidos al perfil de Usuario</h1>
+            <p>Esta es la pagina principal del perfil. Aqui puedes encontrar una breve introducciono bienvenida al perfil del usuario</p>
+        </div>
+    );
+}
+
+export default Home;
